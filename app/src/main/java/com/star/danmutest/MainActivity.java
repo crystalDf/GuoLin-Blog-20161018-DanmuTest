7c3b4f524548ac9554b22dd5cd7bf79c273bab73
crystalDf <chendong333@gmail.com>
2018-05-30 23:47:36 +0800

    DanmuTest

--- app/src/main/java/com/star/danmutest/MainActivity.java
+++ app/src/main/java/com/star/danmutest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.star.danmutest;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/Pixels.mp4");
+        videoView.start();
     }
 }
