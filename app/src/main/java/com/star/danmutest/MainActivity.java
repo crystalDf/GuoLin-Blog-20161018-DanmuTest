c1284ac7b9e3432fe856baa7005b57223d86a5ca
crystalDf <chendong333@gmail.com>
2018-05-31 22:52:17 +0800

    DanmuTest

--- app/src/main/java/com/star/danmutest/MainActivity.java
+++ app/src/main/java/com/star/danmutest/MainActivity.java
@@ -1,8 +1,9 @@
 package com.star.danmutest;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView = findViewById(R.id.video_view);
         videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/Pixels.mp4");
         videoView.start();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+
+            View decorView = getWindow().getDecorView();
+
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
     }
 }
