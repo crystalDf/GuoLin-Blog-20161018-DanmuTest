3e89d68e8bd82b9e29a9c07c009ce41e0ec91f1f
crystalDf <chendong333@gmail.com>
2018-06-03 22:01:26 +0800

    DanmuTest

--- app/src/main/java/com/star/danmutest/MainActivity.java
+++ app/src/main/java/com/star/danmutest/MainActivity.java
@@ -1,11 +1,17 @@
 package com.star.danmutest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.VideoView;
 
+import java.util.Random;
+
+import master.flame.danmaku.controller.DrawHandler;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.DanmakuTimer;
 import master.flame.danmaku.danmaku.model.IDanmakus;
 import master.flame.danmaku.danmaku.model.android.DanmakuContext;
 import master.flame.danmaku.danmaku.model.android.Danmakus;
@@ -32,6 +38,110 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView = findViewById(R.id.video_view);
         videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/Safe & Sound.mp4");
         videoView.start();
+
+        mDanmakuView = findViewById(R.id.danmaku_view);
+        mDanmakuView.enableDanmakuDrawingCache(true);
+        mDanmakuView.setCallback(new DrawHandler.Callback() {
+            @Override
+            public void prepared() {
+                mShowDanmaku = true;
+                mDanmakuView.start();
+                generateSomeDanmaku();
+            }
+
+            @Override
+            public void updateTimer(DanmakuTimer timer) {
+
+            }
+
+            @Override
+            public void danmakuShown(BaseDanmaku danmaku) {
+
+            }
+
+            @Override
+            public void drawingFinished() {
+
+            }
+        });
+
+        mDanmakuContext = DanmakuContext.create();
+        mDanmakuView.prepare(mBaseDanmakuParser, mDanmakuContext);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mDanmakuView != null && mDanmakuView.isPrepared() && mDanmakuView.isPaused()) {
+            mDanmakuView.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mDanmakuView != null && mDanmakuView.isPrepared()) {
+            mDanmakuView.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mShowDanmaku = false;
+
+        if (mDanmakuView != null) {
+            mDanmakuView.release();
+            mDanmakuView = null;
+        }
+    }
+
+    private void generateSomeDanmaku() {
+
+        new Thread(() -> {
+            while (mShowDanmaku) {
+                int time = new Random().nextInt(300);
+
+                String content = "" + time + time;
+
+                addDanmaku(content, false);
+
+                try {
+                    Thread.sleep(time);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void addDanmaku(String content, boolean withBorder) {
+
+        BaseDanmaku baseDanmaku = mDanmakuContext.mDanmakuFactory.createDanmaku(
+                BaseDanmaku.TYPE_SCROLL_RL
+        );
+
+        baseDanmaku.text = content;
+        baseDanmaku.padding = 5;
+        baseDanmaku.textSize = sp2px(20);
+        baseDanmaku.textColor = Color.WHITE;
+        baseDanmaku.setTime(mDanmakuView.getCurrentTime());
+
+        if (withBorder) {
+            baseDanmaku.borderColor = Color.GREEN;
+        }
+
+        mDanmakuView.addDanmaku(baseDanmaku);
+    }
+
+    private int sp2px(float spValue) {
+
+        final float fontScale = getResources().getDisplayMetrics().scaledDensity;
+
+        return (int) (spValue * fontScale + 0.5f);
     }
 
     @Override
