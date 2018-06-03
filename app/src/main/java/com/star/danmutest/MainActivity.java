998c2a8fab9721ce0ebadea6c77db4cc990e506e
crystalDf <chendong333@gmail.com>
2018-06-03 21:42:38 +0800

    DanmuTest

--- app/src/main/java/com/star/danmutest/MainActivity.java
+++ app/src/main/java/com/star/danmutest/MainActivity.java
@@ -6,7 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.VideoView;
 
+import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.android.DanmakuContext;
+import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.ui.widget.DanmakuView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean mShowDanmaku;
+    private DanmakuView mDanmakuView;
+    private DanmakuContext mDanmakuContext;
+    private BaseDanmakuParser mBaseDanmakuParser = new BaseDanmakuParser() {
+        @Override
+        protected IDanmakus parse() {
+            return new Danmakus();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView videoView = findViewById(R.id.video_view);
-        videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/Pixels.mp4");
+        videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/Safe & Sound.mp4");
         videoView.start();
     }
 
